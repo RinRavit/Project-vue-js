1a5248f9a56c367064075f61d7c3b844fab3f281
RinRavit <128680924+RinRavit@users.noreply.github.com>
2023-11-16 14:37:09 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,11 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "/Project-vue-js",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+  
 })
